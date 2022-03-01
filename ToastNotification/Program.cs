4b0cd79055ed12913c0e617f96984a7384a010f9
crize <34015092+gurencrize@users.noreply.github.com>
2022-03-01 20:27:26 +0900

    Update Program.cs
    textの入れ方が良くないらしいので変更
    入力待機はThread.Sleepで誤魔化す

--- ToastNotification/Program.cs
+++ ToastNotification/Program.cs
@@ -1,21 +1,15 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
 using System;
+using System.Threading;
 
-namespace ToastNotification
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var toast = new ToastContentBuilder();
-            toast.AddArgument("action", "viewConversation");
-            toast.AddArgument("conversationId", 9813);
-            foreach (string s in args)
-            {
-                toast.AddText(s);
-            }
-            toast.Show();
-            Console.ReadKey();
-        }
-    }
-}
+string text = string.Join(Environment.NewLine, args);
+var toast = new ToastContentBuilder()
+    .AddArgument("action", "viewConversation")
+    .AddArgument("conversationId", 9813)
+    .AddText("Notification")
+    .AddText(text)
+    .AddButton(new ToastButton()
+        .SetContent("OK")
+        .AddArgument("action", "ok"));
+toast.Show();
+Thread.Sleep(30000);
