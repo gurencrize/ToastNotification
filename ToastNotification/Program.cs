2a25b4b818571e84600ea7a3ebc18e35183d6a07
crize <34015092+gurencrize@users.noreply.github.com>
2022-03-01 19:06:10 +0900

    Initial commit

---
+++ ToastNotification/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+
+namespace ToastNotification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var toast = new ToastContentBuilder();
+            // toast.AddArgument("action", "viewConversation");
+            // toast.AddArgument("conversationId", 9813);
+            foreach(string s in args)
+            {
+                toast.AddText(s);
+            }
+            toast.Show();
+        }
+    }
+}
